da99855fdd4c22d52668bed3945e318cdb8dc44b
Junio Moquiuti <moquiutijunio@Junios-MacBook-Air.local>
2019-12-06 23:14:48 -0400

    made title header view

--- bitcoinquotes/class/BitcoinQuotesViewController.swift
+++ bitcoinquotes/class/BitcoinQuotesViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 import RxSwift
+import Cartography
 
 final class BitcoinQuotesViewController: UIViewController {
 
     private var disposeBag: DisposeBag!
     private lazy var apiClient = APIClient()
     
+    private lazy var titleHeaderView = TitleHeaderView.instantiateFromNib()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        addSubviews()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
+        applyLayout()
         bind()
+    }
+    
+    private func applyLayout() {
+        view.backgroundColor = .white
+        
     }
     
     private func bind() {
@@ -27,11 +42,25 @@ final class BitcoinQuotesViewController: UIViewController {
             .subscribe { (event) in
                 switch event {
                 case .success(let transactions):
-                    print("TODO success \(transactions)")
+                    print("TODO success \(transactions.count)")
                 case .error(let error):
                     print("TODO error \(error)")
                 }
             }
             .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - Layout subviews
+extension BitcoinQuotesViewController {
+    
+    private func addSubviews() {
+        
+        view.addSubview(titleHeaderView)
+        constrain(view, titleHeaderView) { (container, headerView) in
+            headerView.top == container.safeAreaLayoutGuide.top
+            headerView.left == container.left
+            headerView.right == container.right
+        }
     }
 }
---
+++ bitcoinquotes/class/views/titleHeaderView/TitleHeaderView.swift
@@ -0,0 +1,49 @@
+//
+//  TitleHeaderView.swift
+//  bitcoinquotes
+//
+//  Created by Junio Moquiuti on 06/12/19.
+//  Copyright © 2019 Junio Moquiuti. All rights reserved.
+//
+
+import UIKit
+
+class TitleHeaderView: UIView {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var fontLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        applyLayout()
+    }
+    
+    private func applyLayout() {
+        backgroundColor = .clear
+    
+        titleLabel.text = NSLocalizedString("market.price", comment: "")
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 22)
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = .black
+        
+        infoLabel.text = NSLocalizedString("info.blockchain", comment: "")
+        infoLabel.font = UIFont.systemFont(ofSize: 14)
+        infoLabel.numberOfLines = 0
+        infoLabel.textAlignment = .center
+        infoLabel.textColor = .black
+        
+        fontLabel.text = NSLocalizedString("source.blockchain", comment: "")
+        fontLabel.font = UIFont.systemFont(ofSize: 12)
+        fontLabel.textAlignment = .center
+        fontLabel.textColor = .black
+    }
+}
+
+extension TitleHeaderView {
+    
+    class func instantiateFromNib() -> TitleHeaderView {
+        return UINib(nibName: "TitleHeaderView", bundle: nil).instantiate(withOwner: nil, options: nil).first as! TitleHeaderView
+    }
+}
